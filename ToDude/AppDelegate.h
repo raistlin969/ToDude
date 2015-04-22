ffa04642c255637f5d2872868c6c0e8bcdde9dc7
Michael <mdavidson@cala.fsu.edu>
2015-04-22 12:33:30 -0400

    initial

---
+++ ToDude/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDude
+//
+//  Created by Michael Davidson on 4/22/15.
+//  Copyright (c) 2015 Michael Davidson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
